56ecddd7d9c0a4a22f0eb8126b64c105949bbcb5
mb1a <mbarolak+1@gmail.com>
2016-08-20 02:40:03 +0000

    Added a test JSON end point.

--- server/main.py
+++ server/main.py
@@ -1,9 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+    
+@app.route("/base")
+def base_template():
+    return flask.render_template('base.html')
+
+@app.route("/json_test")
+def json_test():
+    d = {'a': 4, 'b': 9}
+    return flask.jsonify(**d)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
+    
