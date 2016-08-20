1fadfc069dec2865a06c3d90300b379ae37822a1
mb1a <mbarolak+1@gmail.com>
2016-08-20 03:25:44 +0000

    Em's engine additions.

--- server/engine.py
+++ server/engine.py
@@ -1,23 +1,44 @@
 #! /usr/bin/env python3
 
 class Player:
-    def __init__(self, name, dice):
+    def __init__(self, name, dice, num_dice):
         self.name = name
         self.dice = dice
+        self.num_dice = num_dice
+        
+    def roll_dice(self):
+        self.dice = []
+        for x in range(0,self.num_dice):
+            self.dice.append(x+1)
+            
+
+            
 
 
 class Game:
     def __init__(self, players, num_dice):
         self.players = []
+        self.num_dice = num_dice
+        self.round_num = 0
+        self.turn_num = 0
+        self.dice = []
+        for x in range(0, self.num_dice):
+            self.dice.append(x)
         for name in players:
-            self.players.append(Player(name, []))
-
+            self.players.append(Player(name, self.dice, self.num_dice))
+        self.current_bet = []
+        
+    def start_round(self):
+        self.round_num += 1
+        for player in self.players:
+            player.roll_dice();
 
 class Engine:
     def __init__(self, random_number_generator):
         self._games = {}
         self._next_id = 1
         self._rng = random_number_generator
+        
 
     def create_game(self, players, num_dice):
         id = self._next_id
--- server/engine_tests.py
+++ server/engine_tests.py
@@ -10,8 +10,19 @@ class TestEngine(unittest.TestCase):
         e = engine.Engine(rng)
         game = e.create_game(['me', 'you'], 3)
 
+
         self.assertEqual('me', game.players[0].name)
         self.assertEqual('you', game.players[1].name)
+        self.assertEqual(3, game.num_dice)
+        self.assertEqual(0, game.turn_num)
+        self.assertEqual(0, game.round_num)
+        self.assertEqual(0, game.players[0].dice[0])
+        self.assertEqual([], game.current_bet)
+        self.assertEqual(3, game.players[0].num_dice)
+        
+        game.start_round()
+        self.assertEqual(1, game.round_num)
+        self.assertEqual(3, game.players[0].dice[2])
 
 if __name__ == '__main__':
     unittest.main()
